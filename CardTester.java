020a4af8765371b1c6f7a1de5ca3e350520a79ad
Dersy2000 <32402583+Dersy2000@users.noreply.github.com>
2018-05-14 13:49:09 -0700

    Initial commit

---
+++ CardTester.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class CardTester here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class CardTester
+{
+    /**
+	 * The main method in this class checks the Card operations for consistency.
+	 *	@param args is not used.
+	 */
+	public static void main(String[] args) {
+		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		Card card = new Card("Jack", "Club", 11);
+	}
+}
